4f07bdd7c31ca1645cfa1cfe68f6fcd989ea523f
sevader14 <vsevolod.deriushkin@gmail.com>
2021-03-18 08:21:27 +0100

    adds visit localhost test

---
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -0,0 +1,7 @@
+// userCanCheckTheirBmi.feature.js
+
+describe('User can check their BMI', () => {
+    it('user can enter weght and height', () => {
+        cy.visit('http://localhost:3001')
+    })
+})
