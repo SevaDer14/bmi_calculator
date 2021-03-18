764a9a6f871b6550563eb1fbca60aa3b92ee911c
sevader14 <vsevolod.deriushkin@gmail.com>
2021-03-18 10:07:29 +0100

    adds health status message & corresponding test

--- src/assets/js/bmiCalculator.js
+++ src/assets/js/bmiCalculator.js
@@ -3,3 +3,15 @@ const calculateMetricBMI = (weight, height) => {
     result = (weight / ((height/100)*(height/100))).toFixed(1)
     return result
 }
+
+const getBMIClassification = (BMI) => {
+    if (BMI < 18.5){
+        return  "Underweight"
+        }else  if (BMI < 25){
+        return  "Healthy"
+        }else  if (BMI <30){
+        return  "Overweight"
+        }else if (BMI >=30){
+            return "Obese"
+        }
+}
--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -3,6 +3,7 @@ const calculateBMI = () => {
     let heightInput = document.getElementById("height")
     let results = calculateMetricBMI(weightInput.value, heightInput.value)
     let displayResultElement = document.getElementById('results')
-    displayResultElement.innerHTML = '<h2>Your BMI value is: '+results+'</h2>'
+    let message = getBMIClassification(results)
+    displayResultElement.innerHTML = '<h2>Your BMI value is: '+results+' and you are '+message+'</h2>'
 }
 
