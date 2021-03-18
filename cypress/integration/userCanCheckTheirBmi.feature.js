01ddef257d9a3a4ce4e7e2858f113d7823af1d17
sevader14 <vsevolod.deriushkin@gmail.com>
2021-03-18 08:36:55 +0100

    adds UI and corresponding tests

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -4,5 +4,8 @@ describe('User can check their BMI', () => {
     it('user can enter weght and height', () => {
         cy.visit('http://localhost:3001')
         cy.contains('BMI Calculator')
+        cy.get('#weight').type('57')
+        cy.get('#height').type('167')
+        cy.get('#Calculate').click()
     })
 })
